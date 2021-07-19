d185b7e0e77410ccddb1fd15fc617e963431d025
developiaa <developiaa@gmail.com>
2021-07-19 23:48:27 +0900

    feat : Test persistence context

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,15 +15,22 @@ public class JpaMain {
 //            Member member = new Member();
 //            member.setId(1L);
 //            member.setName("HelloA");
-            Member member = em.find(Member.class, 1L);
-            System.out.println("member.getId() = " + member.getId());
-            System.out.println("member.getName() = " + member.getName());
-            em.persist(member);
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
+
+//            Member member = em.find(Member.class, 1L);
+//            System.out.println("member.getId() = " + member.getId());
+//            System.out.println("member.getName() = " + member.getName());
+//            em.persist(member);
+//
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+//            for (Member member1 : result) {
+//                System.out.println("member1 = " + member1);
+//            }
+
+            // 영속 상태에 데이터를 동일성을 보장
+            Member member = em.find(Member.class, 1L);
+            Member member1 = em.find(Member.class, 1L);
+            System.out.println("(member == member1) = " + (member == member1));
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
