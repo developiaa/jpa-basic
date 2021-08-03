3b8b662072016d96afad354a7297f495cb4185b7
developiaa <developiaa@gmail.com>
2021-08-03 22:28:01 +0900

    feat : Add embedded type

---
+++ src/main/java/type/Member.java
@@ -0,0 +1,33 @@
+package type;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Embedded
+    private Period workPeriod;
+
+    @Embedded
+    private Address homeAddress;
+
+    @Embedded
+    // 한 엔티티에서 같은 값 타입이 중복될 경우 사용
+    @AttributeOverrides({
+            @AttributeOverride(name = "city",
+                    column = @Column(name = "WORK_CITY")),
+            @AttributeOverride(name = "street",
+                    column = @Column(name = "WORK_STREET")),
+            @AttributeOverride(name = "zipcode",
+                    column = @Column(name = "WORK_ZIPCODE")),
+    })
+    private Address workAddress;
+
+}
