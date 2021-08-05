8f093e249c163b9c2a551ebf5adbd7ad916b0573
developiaa <developiaa@gmail.com>
2021-08-05 21:11:30 +0900

    feat : Add ElementCollection example

--- src/main/java/type/Member.java
+++ src/main/java/type/Member.java
@@ -1,6 +1,10 @@
 package type;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -17,6 +21,15 @@ public class Member {
 
     @Embedded
     private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
 
     @Embedded
     // 한 엔티티에서 같은 값 타입이 중복될 경우 사용
